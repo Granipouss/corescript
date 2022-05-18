3c8808929dc43df5091e1f870084708dd2e9bdd3
Brendan Daoud <brendan.daoud@ubisoft.com>
2022-05-18 08:54:40 +0200

    Use classes for cache

--- js/rpg_core/CacheMap.js
+++ js/rpg_core/CacheMap.js
@@ -1,72 +1,71 @@
-/**
- * Cache for images, audio, or any other kind of resource
- * @param manager
- * @constructor
- */
-
 import { CacheEntry } from './CacheEntry';
 
-export function CacheMap(manager) {
-    this.manager = manager;
-    this._inner = {};
-    this._lastRemovedEntries = {};
-    this.updateTicks = 0;
-    this.lastCheckTTL = 0;
-    this.delayCheckTTL = 100.0;
-    this.updateSeconds = Date.now();
-}
-
 /**
- * checks ttl of all elements and removes dead ones
+ * Cache for images, audio, or any other kind of resource
  */
-CacheMap.prototype.checkTTL = function () {
-    var cache = this._inner;
-    var temp = this._lastRemovedEntries;
-    if (!temp) {
-        temp = [];
-        this._lastRemovedEntries = temp;
+export class CacheMap {
+    constructor(manager) {
+        this.manager = manager;
+        this._inner = {};
+        this._lastRemovedEntries = {};
+        this.updateTicks = 0;
+        this.lastCheckTTL = 0;
+        this.delayCheckTTL = 100.0;
+        this.updateSeconds = Date.now();
     }
-    for (var key in cache) {
-        var entry = cache[key];
-        if (!entry.isStillAlive()) {
-            temp.push(entry);
+
+    /**
+     * checks ttl of all elements and removes dead ones
+     */
+    checkTTL() {
+        var cache = this._inner;
+        var temp = this._lastRemovedEntries;
+        if (!temp) {
+            temp = [];
+            this._lastRemovedEntries = temp;
+        }
+        for (var key in cache) {
+            var entry = cache[key];
+            if (!entry.isStillAlive()) {
+                temp.push(entry);
+            }
+        }
+        for (var i = 0; i < temp.length; i++) {
+            temp[i].free(true);
+        }
+        temp.length = 0;
+    }
+
+    /**
+     * cache item
+     * @param key url of cache element
+     * @returns {*|null}
+     */
+    getItem(key) {
+        var entry = this._inner[key];
+        if (entry) {
+            return entry.item;
+        }
+        return null;
+    }
+
+    clear() {
+        var keys = Object.keys(this._inner);
+        for (var i = 0; i < keys.length; i++) {
+            this._inner[keys[i]].free();
         }
     }
-    for (var i = 0; i < temp.length; i++) {
-        temp[i].free(true);
-    }
-    temp.length = 0;
-};
 
-/**
- * cache item
- * @param key url of cache element
- * @returns {*|null}
- */
-CacheMap.prototype.getItem = function (key) {
-    var entry = this._inner[key];
-    if (entry) {
-        return entry.item;
+    setItem(key, item) {
+        return new CacheEntry(this, key, item).allocate();
     }
-    return null;
-};
 
-CacheMap.prototype.clear = function () {
-    var keys = Object.keys(this._inner);
-    for (var i = 0; i < keys.length; i++) {
-        this._inner[keys[i]].free();
+    update(ticks, delta) {
+        this.updateTicks += ticks;
+        this.updateSeconds += delta;
+        if (this.updateSeconds >= this.delayCheckTTL + this.lastCheckTTL) {
+            this.lastCheckTTL = this.updateSeconds;
+            this.checkTTL();
+        }
     }
-};
-
-CacheMap.prototype.setItem = function (key, item) {
-    return new CacheEntry(this, key, item).allocate();
-};
-
-CacheMap.prototype.update = function (ticks, delta) {
-    this.updateTicks += ticks;
-    this.updateSeconds += delta;
-    if (this.updateSeconds >= this.delayCheckTTL + this.lastCheckTTL) {
-        this.lastCheckTTL = this.updateSeconds;
-        this.checkTTL();
-    }
-};
+}
